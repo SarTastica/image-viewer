6c92efe40dc898fba184578ad358ebcc3e284fdc
SarTastica <sartastica@gmail.com>
2026-01-18 16:18:53 +0000

    SwingImageDeserializer.java class fixed

--- src/main/java/software/ulpgc/imageviewer/view/swing/SwingImageDeserializer.java
+++ src/main/java/software/ulpgc/imageviewer/view/swing/SwingImageDeserializer.java
@@ -4,11 +4,26 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SwingImageDeserializer {
+    private final Map<String, BufferedImage> cache = new HashMap<>();
+
     public BufferedImage deserialize(byte[] bytes) {
+        String key = String.valueOf(java.util.Arrays.hashCode(bytes));
+
+        if (cache.containsKey(key)) {
+            return cache.get(key);
+        }
+
         try {
-            return ImageIO.read(new ByteArrayInputStream(bytes));
+            BufferedImage image = ImageIO.read(new ByteArrayInputStream(bytes));
+
+            if (image != null) {
+                cache.put(key, image);
+            }
+            return image;
         } catch (IOException e) {
             e.printStackTrace();
             return null;
